9d1aac315e9dea56a2d9d8fc36113c3c2b14e711
Matt Leuer <mattleuer@gmail.com>
2019-07-09 16:27:49 -0700

    Initial Commit

---
+++ How Far/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  How Far
+//
+//  Created by Matt Leuer on 7/9/19.
+//  Copyright © 2019 Matt Leuer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
